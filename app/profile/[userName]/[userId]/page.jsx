ade3de1aca25804d504b96fc14589da6a9fb30a2
tuannl1987 <tuannl1987.nlt@gmail.com>
2024-03-06 13:23:54 +0700

    edit link

--- app/profile/[userName]/[userId]/page.jsx
+++ app/profile/[userName]/[userId]/page.jsx
@@ -5,7 +5,7 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useParams } from 'next/navigation'
 
-import Profile from "@components/profile";
+import Profile from "@components/Profile";
 
 const UserProfile = () => {
     // Route -> /profile/[userName]/[userID]
